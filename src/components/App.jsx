aa9c2d5fb84ffed209ef6414c6ed3aafff217bec
tenhold <tenhold@gmail.com>
2020-10-26 12:35:35 -0500

    (Add) react and webpack

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello from App</h1>
+      <h2>welcome team</h2>
+    </>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+const Index = () => {
+  return (
+    <>
+      <App />
+    </>
+  );
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
